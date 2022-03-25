9f4bdab7904d25cc1e06a40a6cebaadf8d2d84e6
Semyon Antoshkin <s.s.antoshkin@gmail.com>
2022-03-25 10:58:44 +0500

    change hosts

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = "u6du%n^fo@o_6)2(h#jv9j%$lqs)4q4f!i+7k0wzowxbxoxm13"
 DEBUG = False
 
 ALLOWED_HOSTS = [
+    "*",
     "localhost",
     "127.0.0.1",
     "[::1]",
@@ -53,9 +54,6 @@ INSTALLED_APPS = [
     "about.apps.AboutConfig",
     "sorl.thumbnail",
 ]
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -142,10 +140,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 LOGIN_URL = "users:login"
 LOGIN_REDIRECT_URL = "posts:index"
